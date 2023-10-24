cce344f2eb862491d55f3d9802c33d1c983618b5
Technostalgic <tteck02@gmail.com>
2023-10-24 03:13:24 -0600

    added assetloader

--- src/asset_plugin.rs
+++ src/asset_plugin.rs
@@ -1,26 +1,56 @@
-use bevy::prelude::*;
-use bevy_common_assets::json::JsonAssetPlugin;
+use bevy::{
+	prelude::*, 
+	asset::{
+		AssetLoader,
+		LoadedAsset
+	}
+};
+use serde_json::from_slice;
 use crate::aseprite_data::SpritesheetData;
 
 pub struct SpritesheetAssetPlugin {
-	file_extensions: Vec<&'static str>
+	extensions: Vec<&'static str>
+}
+
+struct SpritesheetAssetLoader {
+    extensions: Vec<&'static str>
 }
 
 // -----------------------------------------------------------------------------
 
 impl Plugin for SpritesheetAssetPlugin {
 	fn build(&self, app: &mut App) {
-		app.add_plugins(
-			JsonAssetPlugin::<SpritesheetData>::new(&self.file_extensions)
-        );
+        app.add_asset::<SpritesheetData>().add_asset_loader(
+			SpritesheetAssetLoader {
+            	extensions: self.extensions.clone()
+        	}
+		);
 	}
 
 }
 
 impl SpritesheetAssetPlugin {
-	pub fn new(file_extensions: &[&'static str]) -> Self {
+	pub fn new(extensions: &[&'static str]) -> Self {
 		SpritesheetAssetPlugin { 
-			file_extensions: file_extensions.to_owned()
+			extensions: extensions.to_owned()
 		}
 	}
+}
+
+impl AssetLoader for SpritesheetAssetLoader {
+	
+	fn load<'a>(
+		&'a self,
+		bytes: &'a [u8],
+		load_context: &'a mut bevy::asset::LoadContext,
+	) -> bevy::utils::BoxedFuture<'a, Result<(), bevy::asset::Error>> {
+			Box::pin(async move {
+				let asset = from_slice::<SpritesheetData>(bytes)?;
+				// TODO convert to sprite::sheet?
+				load_context.set_default_asset(LoadedAsset::new(asset));
+				Ok(())
+			})
+	}
+
+	fn extensions(&self) -> &[&str] { &self.extensions }
 }
